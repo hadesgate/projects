3ae8bed4b3d72d86e442e644d8ac5d7151084499
Ralph Schra <rschra@ebay.com>
2016-03-02 13:01:08 +0100

    GUI python test

---
+++ guitest.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+
+#create root window
+root = Tk()
+
+# modify root window
+root.title("guis test")
+root.geometry("200x100")
+
+# create a frame in the root window
+app = Frame(root)
+app.grid()
+
+# create some buttons
+	button = Button(app text = "shellserver1")
+button.grid()
+
+# kick off main eventloop
+
+root.mainloop()
