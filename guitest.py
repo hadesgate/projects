4461b091c7acd8d6d95f695274a2da1d0b09b65b
Ralph Schra <rschra@ebay.com>
2016-03-04 11:31:26 +0100

    add buttons and labels

--- guitest.py
+++ guitest.py
@@ -10,18 +10,34 @@ root.geometry("236x100")
 
 # create a frame in the root window
 app = Frame(root)
-app.grid()
-
+app.grid(row=0)
+bapp = Frame(root)
+bapp.grid(row=1)
     
 # create some buttons
-button = Button(root, text = 'shellserver', width=10)
-button.grid(row=0, column=0)
+button = Button(bapp, text = 'shellserver', width=12)
+button.grid(row=0, column=0, sticky=W)
 
-button2 = Button(root, text = 'dnsserver', width=10)
-button2.grid(row=1, column=0)
+button2 = Button(bapp, text = 'dnsserver', width=12)
+button2.grid(row=1, column=0, sticky=W)
 
-button3 = Button(root, text = 'mainserver', width=10)
-button3.grid(row=1, column=1)
+button3 = Button(bapp, text = 'mainserver', width=12)
+button3.grid(row=1, column=1, sticky=W)
+
+# create entries with labels
+
+entry1 = Entry(app)
+entry2 = Entry(app)
+entry1.grid(row=0, column=1)
+entry2.grid(row=1, column=1)
+
+elabel = Label(app, text='Name:')
+elabel2 = Label(app, text='Password:')
+elabel.grid(row=0, column = 0, sticky=E)
+elabel2.grid(row=1, column = 0)
+
+
+
 
 # kick off main eventloop
 # mainly neccessary for Windows users!
